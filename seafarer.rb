6e40348a12a4fc2c187d436ab35b1266d56f5e79
Priyanga-Packirisamy <priyangapackirsamy09@gmail.com>
2022-08-14 13:52:14 +0530

    changed password verification method

--- seafarer.rb
+++ seafarer.rb
@@ -11,23 +11,57 @@ else
     puts "Enter valid INDOS no."
 end
 
+uname = gets
+Password = gets
+uc = uname[2..3].scan(/[A-Z]/).length
+dc = uname.scan(/[0-9]/).length
+if uc == 2 && dc == 6
+    puts "username : #{uname}"
+else
+    puts "Enter valid INDOS no."
+end
+
 if Password.length >= 9 && Password.length <=13
     uc = Password.scan(/[A-Z]/).length
     lc = Password.scan(/[a-z]/).length
     dc = Password.scan(/[0-9]/).length
-    sc = Password.scan(/["+", "-", "&", "|", "!", "(", ")", "{", "}", "[", "]", "^","", "*", "?", ":","@","","#","$","%","*","_","=","\","<",">"]/).length
-    if sc >= dc && sc>= uc && sc >= lc
+    sc = Password.scan(/["+", "-", "&", "|", "!", "(", ")", "{", "}", "[", "]", "^","~", "*", "?", ":","@","~","#","$","%","*","_","=","\","<",">"]/).length
+   
+    if sc>=1 && dc>=1 && lc>=1 && uc>=1
         puts "Strong password"
-        puts "Password : #{Password}"
-    elsif dc >= sc && dc >= uc && dc >= lc 
-        puts "Good password"
-        puts "Password : #{Password}"
-    elsif uc >= sc && uc >= dc && uc >= lc
-        puts "Weak password"
         puts "Password : #{Password}"
     else
         puts "Invalid password"
-        puts "Password : #{Password}"
+        if sc<1 && dc<1 && lc<1 && uc<1
+            puts "Password should have atleast 1 symbol,1 number,1 lowercase alphabet and 1 uppercase alphabet"
+        elsif sc<1 && dc<1 && lc<1  
+            puts "Password should have atleast 1 symbol,1 number and 1 lowercase alphabet"
+        elsif sc<1 && dc<1 && uc<1
+            puts "Password should have atleast 1 symbol,1 number and 1 uppercase alphabet"
+        elsif sc<1  && lc<1 && uc<1
+            puts "Password should have atleast 1 symbol,1 lowercase alphabet and 1 uppercase alphabet"
+        elsif dc<1 && lc<1 && uc<1
+            puts "Password should have atleast 1 number,1 lowercase alphabet and 1 uppercase alphabet"
+        elsif sc<1 && dc<1
+            puts "Password should have atleast 1 symbol and 1 number"
+        elsif sc<1  && lc<1
+            puts "Password should have atleast 1 symbol and 1 lowercase alphabet"
+        elsif  dc<1 && lc<1
+            puts "Password should have atleast 1 number and 1 lowercase alphabet"
+        elsif sc<1 &&  uc<1
+            puts "Password should have atleast 1 symbol and 1 uppercase alphabet"
+        elsif  dc<1 && uc<1
+            puts "Password should have atleast 1 number and 1 uppercase alphabet"
+        elsif lc<1 && uc<1
+            puts "Password should have atleast 1 lowercase alphabet and 1 uppercase alphabet"
+        elsif sc<1
+            puts "Password should have atleast 1 symbol"
+        elsif  dc<1
+            puts "Password should have atleast 1 number "
+        elsif lc<1  
+            puts "Password should have atleast 1 lowercase alphabet"
+        else
+            puts "Password should have atleast 1 uppercase alphabet"
     end
 else
     puts "Retype password of greater than 8 characters and less than 12 characters"
